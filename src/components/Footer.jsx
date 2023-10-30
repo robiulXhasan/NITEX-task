ee4d30fbb5ac50e013a0d771008c8a51555c1cb5
robiulXhasan <robi16275@gmail.com>
2023-10-30 22:27:40 +0600

    navbar and footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,42 @@
+import { AiOutlineFacebook, AiOutlineInstagram, AiOutlineLinkedin, AiOutlineMail, AiOutlinePhone, AiOutlineYoutube } from 'react-icons/ai';
 
 const Footer = () => {
-    return (
-        <div className="">
-            This is footer
-        </div>
-    );
+  return (
+    <div className=" ">
+      <footer className="footer  bg-purple-100 text-base-content py-10 px-[8%] text-[16px]">
+        <aside>
+         
+         
+          <a className="normal-case text-2xl font-bold">M<span className="text-yellow-500">G</span>S<span className="text-yellow-500">C</span></a>
+            <p className="text-xs">Morning Glory School & Collage</p>
+           <p className="font-medium">Providing best study since 2014</p>
+          
+        </aside>
+        <nav>
+          <header className="footer-title">Navigation</header>
+          <a className="hover:text-yellow-500 cursor-pointer">Admission</a>
+          <a className="hover:text-yellow-500 cursor-pointer">Apply Online</a>
+          <a className="hover:text-yellow-500 cursor-pointer">Teachers</a>
+          <a className="hover:text-yellow-500 cursor-pointer">About us</a>
+      
+        </nav>
+        <nav>
+          <header className="footer-title">Important Links</header>
+          <a className="hover:text-yellow-500 cursor-pointer">Class Routine</a>
+          <a className="hover:text-yellow-500 cursor-pointer">Photo Archived</a>
+          <a className="hover:text-yellow-500 cursor-pointer">Blogs</a>
+          <a className="hover:text-yellow-500 cursor-pointer">FAQ</a>
+        </nav>
+        
+        <nav>
+          <header className="footer-title">Contact</header>
+          <div className="link link-hover flex items-center text-[16px]"><AiOutlineMail className='text-2xl '/>:  info@mgsc.com</div>
+          <div className=" flex items-center text-[16px] "><AiOutlinePhone className='text-2xl '/>: 01882345960 </div>
+          <div className="link link-hover flex items-center text-2xl gap-4 "> <AiOutlineFacebook/><AiOutlineYoutube/><AiOutlineLinkedin/> <AiOutlineInstagram/> </div>
+        </nav>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,91 @@
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <div>
-            This is nabar
+    const navItems = (
+        <>
+         
+          <li>
+            <Link to="academics" activeClass="active"  className="cursor-pointer">
+            Academics
+            </Link>
+          </li>
+          <li>
+            <Link to="facilities" activeClass="active" className="cursor-pointer">
+              Facilities
+            </Link>
+          </li>
+         
+          <li>
+            <Link
+              to="admission"
+              activeClass="active"
+              smooth={true}
+              spy={true}
+              className="cursor-pointer"
+            >
+              Admission
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="highlights"
+              activeClass="active"
+              
+              className="cursor-pointer"
+            >
+              Highlights
+            </Link>
+          </li>
+          <li>
+            <Link to="about" activeClass="active" smooth={true} spy={true} className="cursor-pointer">
+              About Us
+            </Link>
+          </li>
+          <li>
+            <Link to="contact" activeClass="active" smooth={true} spy={true} className="cursor-pointer">
+              Contact Us
+            </Link>
+          </li>
+         
+        </>
+  );
+  return (
+    <div className="navbar bg-base-200 px-[8%]">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {navItems}
+          </ul>
         </div>
-    );
+        <a className="normal-case text-2xl font-bold">M<span className="text-yellow-500">G</span>S<span className="text-yellow-500">C</span></a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 text-[16px] font-medium">{navItems}</ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn btn-outline">Apply Online</a>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
